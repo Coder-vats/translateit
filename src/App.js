9d2f4dbd41bc572b97c0830a6d40c3cf80dd4020
vatsal <vatsalrastogi1510@gmail.com>
2023-02-05 17:53:22 +0530

    TranslateIt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,148 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Arrows from "./components/Arrows";
+import Button from "./components/Button";
+import Modal from "./components/Modal";
+import TextBox from "./components/TextBox";
+import axios from "axios";
+import { Key, Host } from "./api";
+
+
+
 
 function App() {
+
+  const [showModal, setShowModal] = useState(null);
+
+  const [iCode, setIcode] = useState("en");
+  const [oCode, setOcode] = useState("hi");
+  const [inputLanguage, setInputLanguage] = useState("English");
+  const [outputLanguage, setOutputLanguage] = useState("Hindi");
+  const [textToTranslate, setTextToTranslate] = useState("");
+  const [translatedText, setTranslatedText] = useState("");
+
+  const [languages, setLanguages] = useState(null);
+
+
+
+  const getLanguages = async () => {
+
+    const options = {
+      method: 'GET',
+      url: 'https://text-translator2.p.rapidapi.com/getLanguages',
+      headers: {
+        'X-RapidAPI-Key': Key,
+        'X-RapidAPI-Host': Host
+      }
+    };
+
+
+
+    axios.request(options).then(function (response) {
+      setLanguages(response.data.data.languages);
+    }).catch(function (error) {
+      console.error(error);
+    });
+
+
+  }
+
+  useEffect(() => {
+    getLanguages()
+  }, []);
+
+
+
+  const translate = () => {
+    const encodedParams = new URLSearchParams();
+    encodedParams.append("source_language", iCode);
+    encodedParams.append("target_language", oCode);
+    encodedParams.append("text", textToTranslate);
+
+    const options = {
+      method: 'POST',
+      url: 'https://text-translator2.p.rapidapi.com/translate',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'X-RapidAPI-Key': Key,
+        'X-RapidAPI-Host': Host
+      },
+      data: encodedParams
+    };
+
+    axios.request(options).then(function (response) {
+      setTranslatedText(response.data.data.translatedText)
+    }).catch(function (error) {
+      console.error(error);
+    });
+
+  }
+
+
+
+  const handleClick = () => {
+    setInputLanguage(outputLanguage);
+    setOutputLanguage(inputLanguage);
+  }
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {!showModal && <>
+        <TextBox
+          style="input"
+          selectedLanguage={inputLanguage}
+          setShowModal={setShowModal}
+          setTextToTranslate={setTextToTranslate}
+          textToTranslate={textToTranslate}
+          setTranslatedText={setTranslatedText}
+
+        />
+
+        <div className="arrow-container" >
+          <div className="arrow-container" onClick={handleClick}>
+            <Arrows />
+          </div>
+
+          <div className="button-container" onClick={translate}>
+            <Button />
+
+          </div>
+        </div>
+
+
+
+
+
+        <TextBox
+          style="output"
+          selectedLanguage={outputLanguage}
+          setShowModal={setShowModal}
+          translatedText={translatedText}
+
+        />
+
+
+
+      </>
+      }
+
+      {showModal
+        &&
+        <Modal
+          setShowModal={setShowModal}
+          languages={languages}
+          chosenLanguage={showModal === 'input' ? inputLanguage : outputLanguage}
+          setChosenLanguage={showModal === 'input' ? setInputLanguage : setOutputLanguage}
+          setCode={showModal === 'input' ? setIcode : setOcode}
+        />
+
+
+      }
+
     </div>
   );
 }
